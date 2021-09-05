2785d96d96e7bfa8c6b35971c5e7b12622e731e6
thaisascastro <thaisascastro@gmail.com>
2021-09-05 13:27:03 -0300

    definindo carousel

--- web/src/components/card-carousel/carousel.component.tsx
+++ web/src/components/card-carousel/carousel.component.tsx
@@ -29,8 +29,8 @@ export const CardCarousel = () => {
             itemsToShow={itemsPerPage}
             focusOnSelect={true}
             enableAutoPlay
-            isRTL={false}
-            showArrows={false}
+            isRTL={true}
+            showArrows={true}
             enableSwipe={true}
             ref={carouselRef}
             autoPlaySpeed={2000}
@@ -43,36 +43,45 @@ export const CardCarousel = () => {
               }
             }}
           >
-            <img height={400} width={500} src="/assets/bands/bon-jovi.jpg" />
-            <img height={400} width={500} src="/assets/bands/guns.jpeg" />
-            <img height={400} width={500} src="/assets/bands/iron.jpeg" />
-            <img height={400} width={500} src="/assets/bands/metallica.jpeg" />
-            <img height={400} width={500} src="/assets/bands/pink-floyd.jpeg" />
-            <img height={400} width={500} src="/assets/bands/rolling.jpeg" />
-            <img height={400} width={500} src="/assets/bands/SOAD.jpeg" />
+            <img height={400} width={470} src="/assets/bands/bon-jovi.jpg" />
+            <img height={400} width={450} src="/assets/bands/guns.jpeg" />
+            <img height={400} width={430} src="/assets/bands/iron.jpeg" />
+            <img height={400} width={470} src="/assets/bands/metallica.jpeg" />
+            <img height={400} width={470} src="/assets/bands/pink-floyd.jpeg" />
+            <img height={400} width={470} src="/assets/bands/rolling.jpeg" />
+            <img height={400} width={470} src="/assets/bands/SOAD.jpeg" />
           </Carousel>
-          {/* <Carousel responsive={{}}>
-              <img
-                height={500}
-                width={500}
-                src="/assets/bands/aerosmith.jpeg"
-              />
-              <img height={500} width={500} src="/assets/bands/bon-jovi.jpg" />
-              <img height={500} width={500} src="/assets/bands/guns.jpeg" />
-              <img height={500} width={500} src="/assets/bands/iron.jpeg" />
-              <img
-                height={500}
-                width={500}
-                src="/assets/bands/metallica.jpeg"
-              />
-              <img
-                height={500}
-                width={500}
-                src="/assets/bands/pink-floyd.jpeg"
-              />
-              <img height={500} width={500} src="/assets/bands/rolling.jpeg" />
-              <img height={500} width={500} src="/assets/bands/SOAD.jpeg" />
-            </Carousel> */}
+        </div>
+        <div className="promo">
+          <span>Venha fazer parte disto!</span>
+        </div>
+        <div className="days">
+          <a href="/contact">
+            <div>
+              <span style={{ marginRight: 15 }}>SOAD</span>
+              {/* <ul>2 de Setembro</ul> */}
+            </div>
+            <div>
+              <span style={{ marginRight: 15 }}>Rolling Stones</span>
+              {/* <ul>3 de Setembro</ul> */}
+            </div>
+            <div>
+              <span>Roger Waters</span>
+              {/* <ul>4 de Setembro</ul> */}
+            </div>
+            <div>
+              <span style={{ marginLeft: 15 }}>Metallica</span>
+              {/* <ul>8 de Setembro</ul> */}
+            </div>
+            <div>
+              <span style={{ marginLeft: 15 }}>Iron Maiden</span>
+              {/* <ul>9 de Setembro</ul> */}
+            </div>
+            <div>
+              <span style={{ marginLeft: 25 }}>Guns N' Roses</span>
+              {/* <ul>10 de Setembro</ul> */}
+            </div>
+          </a>
         </div>
       </div>
     </Fragment>
